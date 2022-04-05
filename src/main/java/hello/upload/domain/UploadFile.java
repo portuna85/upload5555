ff9f670ccb151e449b9647d7ae7804decda81ade
portuna85 <portuna85@gmail.com>
2022-04-05 15:36:37 +0900

    File Upload

--- src/main/java/hello/upload/domain/UploadFile.java
+++ src/main/java/hello/upload/domain/UploadFile.java
@@ -6,4 +6,9 @@ import lombok.Data;
 public class UploadFile {
     private String uploadFileName;
     private String storeFileName;
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
 }
